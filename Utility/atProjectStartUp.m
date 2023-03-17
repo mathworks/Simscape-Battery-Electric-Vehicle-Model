b73057d11ad3e7eda40c9b2ec21ef10fe5acd3d1
Isaac Ito <isaacito@mathworks.com>
2023-03-17 16:16:21 +0000

    Update screenshots. Check MATLAB release with R2023a.

--- Utility/atProjectStartUp.m
+++ Utility/atProjectStartUp.m
@@ -6,7 +6,7 @@ function atProjectStartUp
 %
 % Copyright 2020-2023 The MathWorks, Inc.
 
-if not(contains(string(ver('matlab').Release), "R2022b"))
+if not(contains(string(ver('matlab').Release), "R2023a"))
   disp("This project was developed in R2022b.")
   relstr = ver('matlab').Release;
   disp("This MATLAB Release is " + relstr(2:end-1) + ".")
